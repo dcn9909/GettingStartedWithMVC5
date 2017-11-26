bbb7f70dffbe45bc834284e254f45e0192a06803
Đặng Huỳnh Đạt Ý <dhdatyit.sgu@gmail.com>
2017-11-27 01:33:31 +0700

    Add files and folders

---
+++ MvcMovie/App_Start/RouteConfig.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Routing;
+
+namespace MvcMovie
+{
+    public class RouteConfig
+    {
+        public static void RegisterRoutes(RouteCollection routes)
+        {
+            routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
+
+            routes.MapRoute(
+                name: "Default",
+                url: "{controller}/{action}/{id}",
+                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+            );
+
+            // Route này dùng cho 2 params là name và id
+            routes.MapRoute(
+                name: "Hello",
+                url: "{Controller}/{action}/{name}/{id}"
+            );
+        }
+    }
+}
